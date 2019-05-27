1cfd213ec8ab3678865ca7446073ade6133d7af2
michaelzheng1 <michaelzheng0128@gmail.com>
2019-05-26 20:55:43 -0400

    Updates

---
+++ precache-manifest.0682cc644a5d326f83be83be380fcf34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b33e70cbf52f15c0efb1a21fe6c1edb",
+    "url": "/michaelzheng1/react-exercise/tree/master/ravenous/index.html"
+  },
+  {
+    "revision": "311c0f19122f25358d8c",
+    "url": "/michaelzheng1/react-exercise/tree/master/ravenous/static/css/main.c08f20da.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/michaelzheng1/react-exercise/tree/master/ravenous/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "311c0f19122f25358d8c",
+    "url": "/michaelzheng1/react-exercise/tree/master/ravenous/static/js/main.9eb6e074.chunk.js"
+  },
+  {
+    "revision": "bca061848c5b5834dbd7",
+    "url": "/michaelzheng1/react-exercise/tree/master/ravenous/static/js/runtime~main.1d0d4dbb.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/michaelzheng1/react-exercise/tree/master/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/michaelzheng1/react-exercise/tree/master/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
